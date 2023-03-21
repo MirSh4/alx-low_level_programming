22fb223e31c1d129a141d49e023f93bcc307ccf8
Mirna <mirnasherif420@example.com>
2023-03-21 11:27:18 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+include "main.h"
+/**
+ * main is entry point
+ * prints _putchar to std out
+ * returns :always 0
+ */
+int main(void)
+{
+       	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('\n');
+	_putchar('0');
+	return (0);
+}
