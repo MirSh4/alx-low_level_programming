9394e7b6f25c344a47a5d23bc21bb24f4435ca2a
Mirna <mirnasherif420@example.com>
2023-03-21 12:38:00 -0700

    why am i using betty?

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - prints alphabets.
+ * print_alphabet	-	 prints alphabets.
  * 
  * Return: Always 0
  */
@@ -8,7 +8,7 @@ void print_alphabet(void)
 {
 	int i;
 
-	for (i = 98; i <= 122; i++)
+	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
 	}
