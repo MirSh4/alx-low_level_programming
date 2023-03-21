147a1b269a12bcd2977cd408a95e859f5aec6ff1
Mirna <mirnasherif420@example.com>
2023-03-21 12:48:24 -0700

    alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ *
+ * Return : always zero
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+	
+	for (i= 0; i< 10; i++)
+	{
+		for (j= 98; j< 122; j++)
+		{
+			_putchar(j);
+		}
+	}
+}	
