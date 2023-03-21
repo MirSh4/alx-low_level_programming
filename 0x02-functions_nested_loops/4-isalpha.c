a277e6c123d0c21ad96453aa7ea984145b6ea451
Mirna <mirnasherif420@example.com>
2023-03-21 16:08:27 -0700

    is alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha - Shows 1 if input is letter and zero otherwise
+ *
+ * @c: is character
+ *
+ * Return: 1 for letters ,zero otheriwse.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 41 && c <= 122) || (c >=65 && c <= 90))
+	{
+		return	(1);
+	}
+	return	(0);
+}
